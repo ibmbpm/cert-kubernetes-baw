ba339f5691d9b01979d91d4dd7bbe5b2a55bb2ef
Ananthram J <ananthram.j@ibm.com>
2026-02-09 14:35:48 +0530

    Fixing CS version in common.sh

--- scripts/helper/common.sh
+++ scripts/helper/common.sh
@@ -215,9 +215,9 @@ CP4BA_CSV_VERSION="v25.0.4"
 # CP4BA_CHANNEL_VERSION is for switch CP4BA operator upgrade status, need to update for major release
 CP4BA_CHANNEL_VERSION="v25.0"
 # CS_OPERATOR_VERSION is for checking CPFS operator upgrade status, need to update for each IFIX
-CS_OPERATOR_VERSION="v4.16.0"
+CS_OPERATOR_VERSION="v4.17.0"
 # CS_CHANNEL_VERSION is for for CPFS script -c option, need to update for each IFIX
-CS_CHANNEL_VERSION="v4.16"
+CS_CHANNEL_VERSION="v4.17"
 # CS CHANNEL VERSION that is used in the KC
 CS_CHANNEL_KC="4.x_cd"
 # CERT_LICENSE_OPERATOR_VERSION is for checking IBM cert-manager/licensing operator upgrade status, need to update for each IFIX
