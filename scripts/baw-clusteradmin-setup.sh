e3ced82902bce7eced9718d6ab614755f1a48fc5
Ananthram J <ananthram.j@ibm.com>
2026-02-10 23:37:24 +0530

    Fixing catalog source operator image

--- scripts/baw-clusteradmin-setup.sh
+++ scripts/baw-clusteradmin-setup.sh
@@ -1391,7 +1391,7 @@ function catalog_source_staging() {
         # For dev mode the image for the catalog source has to be in cp.stg.icr.io and a secrets field has to be added
         if [[ "$RUNTIME_MODE" == "dev" ]]; then
             # temporarily adding ibm-zen-operator-catalog because as of March 13th 2025 zen has not GAed
-            if [[ "$name" == "ibm-cp4a-operator-catalog" || "$name" == "ibm-fncm-operator-catalog"  || "$name" == "ibm-zen-operator-catalog-6-2-0" || "$name" == "cloud-native-postgresql-catalog" || "$name" == "ibm-cp-automation-catalog" ]]; then
+            if [[ "$name" == "ibm-cp4a-operator-catalog" || "$name" == "ibm-fncm-operator-catalog"  || "$name" == "ibm-zen-operator-catalog" || "$name" == "cloud-native-postgresql-catalog" || "$name" == "ibm-cp-automation-catalog" ]]; then
                 # Extract the current image value
                 current_image=$(${YQ_CMD} r -d "$((doc_index - 1))" "$OLM_CATALOG_TMP" 'spec.image')
 
