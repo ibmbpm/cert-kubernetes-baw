1cf46ba4a900f4e670763ff0c8e5ca8976d40ae4
Arjun <arjun@Arjuns-MacBook-Pro-2.local>
2026-04-10 13:52:16 +0530

    updated the required cs catalog version

--- scripts/helper/common.sh
+++ scripts/helper/common.sh
@@ -225,7 +225,7 @@ CERT_LICENSE_OPERATOR_VERSION="v4.2.21"
 # CERT_LICENSE_CHANNEL_VERSION is for for IBM cert-manager/licensing script -c option, need to update for each IFIX
 CERT_LICENSE_CHANNEL_VERSION="v4.2"
 # CS_CATALOG_VERSION is for CPFS script -s option, need to update for each IFIX
-CS_CATALOG_VERSION="ibm-cs-install-catalog-v4-17-0"
+CS_CATALOG_VERSION="ibm-cs-install-catalog-v4-18-0"
 # ZEN_OPERATOR_VERSION is for checking ZenService operator upgrade status, need to update for each IFIX
 ZEN_OPERATOR_VERSION="v6.4.1"
 # BTS_CHANNEL_VERSION is for for BTS, need to update for each IFIX
